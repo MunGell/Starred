8c4d4a0e5183f1e1a539906624e2c1a143528d81
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-06-15 21:13:21 +0100

    Add Repository model and migration

---
+++ app/Repository.php
@@ -0,0 +1,77 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use \DB;
+
+/**
+ * Class Repository
+ * @todo add repository starred date
+ * @package App
+ */
+class Repository extends Model
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'repositories';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id', 'name', 'full_name', 'url', 'description'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
+
+    public function allTags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
+
+    public function tags($user_id)
+    {
+        $repository_id = $this->id;
+
+        $tags = DB::table('users')
+            ->where('users.id', '=', $user_id)
+            ->join('repository_user', function ($join) use ($repository_id) {
+                $join->on('repository_user.user_id', '=', 'users.id')
+                    ->where('repository_user.repository_id', '=', $repository_id);
+            })
+            ->join('repository_tag', 'repository_tag.repository_id', '=', 'repository_user.repository_id')
+            ->join('tags', 'repository_tag.tag_id', '=', 'tags.id')
+            ->select('tags.id', 'tags.title')
+            ->get();
+
+        return Collection::make($tags)->sortBy('title');
+    }
+
+}
---
+++ database/migrations/2015_04_20_220222_create_repositories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepositoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('repositories', function (Blueprint $table) {
+            $table->integer('id')->unique();
+            $table->string('name');
+            $table->string('full_name');
+            $table->string('url');
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('repositories');
+    }
+
+}
