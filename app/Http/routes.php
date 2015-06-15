16dd8cfa9115d5b22e314e868c01c88248c45e85
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-06-15 21:24:30 +0100

    Update routes to the new controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+]);
+
+Route::get('/', 'HomeController@index');
+
+Route::get('home', 'HomeController@index');
+
+Route::get('sync', 'SyncController@index');
+
+Route::get('repositories', 'RepositoryController@index');
+Route::post('repositories/{id}/tags/add', 'RepositoryController@addTag')->where('id', '[0-9]+');
+Route::post('repositories/{id}/tags/remove', 'RepositoryController@removeTag')->where('id', '[0-9]+');
+Route::get('repositories/{id}', 'RepositoryController@show')->where('id', '[0-9]+');
+
+Route::get('tags', 'TagController@index');
+Route::get('tags/{id}', 'TagController@show')->where('id', '[0-9]+');
