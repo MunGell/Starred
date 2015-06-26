c0a5e8474e85c369b30042ac601260e5d16d4486
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-06-27 00:26:33 +0100

    Update repository controller to 5.1

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -43,7 +43,7 @@ class RepositoryController extends Controller
     public function removeTag($id)
     {
         $tag_id = Input::get('tag');
-        $tag = Tag::find($tag_id);
+        $tag = Tag::find($tag_id)->first();
         $tag->repositories()->detach($id);
 
         return 'true';
