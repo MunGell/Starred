528558d2d63a8146eb8d04d36cbac3c7a7da5a6f
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-06-26 08:02:59 +0100

    Update repository page to add tags

--- resources/assets/js/pages/RepositoryPage.js
+++ resources/assets/js/pages/RepositoryPage.js
@@ -1,6 +1,8 @@
 import $ from 'jquery'
 import React from 'react'
 
+import Cookies from '../utils/cookies'
+
 import TagManager from '../components/tag-manager'
 
 export default React.createClass({
@@ -22,12 +24,22 @@ export default React.createClass({
         }
     },
 
+    _onTagAdd: function(data, callback) {
+        $.ajaxSetup({
+            headers: {
+                'X-XSRF-TOKEN': Cookies.getCookie('XSRF-TOKEN')
+            }
+        });
+
+        $.post('/repositories/' + this.state.id + '/tags/add', data, callback);
+    },
+
     render: function () {
         return (
             <div className="page-repository">
                 <h1>{this.state.name}</h1>
                 <h3>{this.state.description}</h3>
-                <TagManager tags={this.state.tags} />
+                <TagManager tags={this.state.tags} onTagAdd={this._onTagAdd}/>
             </div>
         )
     }
