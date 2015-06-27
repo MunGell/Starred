53a7f71e3477546d4885c500cb67c6f3f1e4c9ba
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-06-27 16:27:55 +0100

    Add tag page

---
+++ resources/assets/js/pages/TagPage.js
@@ -0,0 +1,33 @@
+import $ from 'jquery'
+import React from 'react'
+
+import RepositoryList from '../components/repository-list'
+
+export default React.createClass({
+
+    getInitialState: function () {
+        return {
+            data: []
+        }
+    },
+
+    componentDidMount: function () {
+        $.get('/tags/' + this.props.data.id)
+            .done(this._setData);
+    },
+
+    _setData: function (data) {
+        if (this.isMounted()) {
+            this.setState(data);
+        }
+    },
+
+    render: function () {
+        return (
+            <div className="page-tag">
+                <RepositoryList data={this.state.data} root='/tags/' />
+            </div>
+        )
+    }
+
+});
