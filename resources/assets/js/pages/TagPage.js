c8986ca617e36eba6dd2312d40f814b8fc418d32
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-06-28 14:29:58 +0100

    Change Repositories URL root

--- resources/assets/js/pages/TagPage.js
+++ resources/assets/js/pages/TagPage.js
@@ -25,7 +25,7 @@ export default React.createClass({
     render: function () {
         return (
             <div className="page-tag">
-                <RepositoryList data={this.state.data} root='/tags/' />
+                <RepositoryList data={this.state.data} root='/repositories/' />
             </div>
         )
     }
