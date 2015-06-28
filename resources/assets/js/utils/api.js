188a28e4695692222060b2112a540791395ea2c1
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-06-28 14:58:31 +0100

    Add API utility module

---
+++ resources/assets/js/utils/api.js
@@ -0,0 +1,43 @@
+import $ from 'jquery'
+import Cookies from '../utils/cookies'
+
+function Api() {
+    $.ajaxSetup({
+        headers: {
+            'X-XSRF-TOKEN': Cookies.getCookie('XSRF-TOKEN')
+        }
+    });
+}
+
+Api.prototype.get = function (url, callback) {
+    $.get(url)
+        .done(callback)
+        .fail(this._onError);
+};
+
+Api.prototype.post = function (url, data, callback) {
+    $.post(url, data)
+        .done(callback)
+        .fail(this._onError);
+};
+
+Api.prototype.getRepository = function (id, callback) {
+    this.get('/repositories/' + id, callback);
+};
+
+Api.prototype.addTag = function (id, data, callback) {
+    this.post('/repositories/' + id + '/tags/add', data, callback);
+};
+
+Api.prototype.removeTag = function (id, data, callback) {
+    this.post('/repositories/' + id + '/tags/remove', data, callback);
+};
+
+Api.prototype._onError = function(response) {
+    if (response.status === 401) {
+        // @todo: add return url in here
+        window.location = '/auth/login';
+    }
+};
+
+export default new Api();
