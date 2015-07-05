4d3c310df9f97b6110b14dbc9a59f9379bf91d1b
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-07-05 21:03:32 +0100

    Add tag-list component

---
+++ resources/assets/js/components/tag-list/index.js
@@ -0,0 +1,27 @@
+import $ from 'jquery'
+import React from 'react'
+
+export default React.createClass({
+
+    getDefaultProps: function () {
+        return {
+            data: []
+        }
+    },
+
+    render: function () {
+        return (
+            <div className="component-tag-list">
+                <ul className="component-tag-list__list">
+                {this.props.data.map(function (value) {
+                    return (
+                        <li className="component-tag-list__list__item" key={value.id}>
+                            <a href={'#' + this.props.root + value.id}>{value.title}</a>
+                        </li>
+                    )
+                }.bind(this))}
+                </ul>
+            </div>
+        )
+    }
+});
