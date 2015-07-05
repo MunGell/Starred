45fa9598295dd95803b97f5cce5d8961e126ffcf
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-07-05 21:03:21 +0100

    Remove unused dependencies from paginator files

--- resources/assets/js/components/paginator/index.js
+++ resources/assets/js/components/paginator/index.js
@@ -1,5 +1,4 @@
 import $ from 'jquery'
-import _ from 'lodash'
 import React from 'react'
 
 import Simple from './simple'
--- resources/assets/js/components/paginator/simple.js
+++ resources/assets/js/components/paginator/simple.js
@@ -1,5 +1,4 @@
 import $ from 'jquery'
-import _ from 'lodash'
 import React from 'react'
 
 export default React.createClass({
