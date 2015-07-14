5be34a96ddfebc7cff12ccfabc23ae881bc1a329
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-07-14 22:37:47 +0100

    Remove bootstrap styles from tag manager form

--- resources/assets/js/components/tag-manager/form.js
+++ resources/assets/js/components/tag-manager/form.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default React.createClass({
 
-    _onSubmit: function(event) {
+    _onSubmit: function (event) {
         event.preventDefault();
         $(document).trigger('component.tag-manager.list.add', {
             title: event.currentTarget[0].value
@@ -14,14 +14,9 @@ export default React.createClass({
     render: function () {
         return (
             <div className="component__tag-manager__form">
-                <form className="form-inline" onSubmit={this._onSubmit}>
-                    <div className="input-group">
-                        <label for="tagInput" className="sr-only">Add a tag</label>
-                        <input type="text" name="title" className="form-control" id="tagInput" placeholder="Add a tag" />
-                        <span className="input-group-btn">
-                            <button className="btn btn-default">Add</button>
-                        </span>
-                    </div>
+                <form onSubmit={this._onSubmit}>
+                    <label for="tagInput" className="sr-only">Add a tag</label>
+                    <input type="text" name="title" className="form-control" id="tagInput" placeholder="Add a tag"/>
                 </form>
             </div>
         )
