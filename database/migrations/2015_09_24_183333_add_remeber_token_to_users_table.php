a539adfb38ea4d7044e74ec05e65c3b7831defef
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-09-24 19:42:14 +0100

    Add migration to accommodate remember_token value in users table

---
+++ database/migrations/2015_09_24_183333_add_remeber_token_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRemeberTokenToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->rememberToken()->after('avatar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('remember_token');
+        });
+    }
+}
