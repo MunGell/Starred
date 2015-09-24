977f5918bf00cdbe6f9a2abedc8fd31ff30b826f
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-09-24 20:17:16 +0100

    Add landing page structure

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -2,13 +2,14 @@ import $ from 'jquery'
 import React from 'react'
 import RoutePattern from 'route-pattern'
 
+import LandingPage from './pages/LandingPage'
 import RepositoriesPage from './pages/RepositoriesPage'
 import RepositoryPage from './pages/RepositoryPage'
 import SearchPage from './pages/SearchPage'
 import TagPage from './pages/TagPage'
 
 var Routes = {
-    '*': RepositoriesPage,
+    '*': LandingPage,
     'search': SearchPage,
     'search/?page=:page': SearchPage,
     'repositories': RepositoriesPage,
