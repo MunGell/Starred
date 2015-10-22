3ae2c0d7f1b48c38853ff53bf8216ab6808930fb
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-10-22 22:25:49 +0100

    Add migration for user_job table

---
+++ database/migrations/2015_10_22_204750_create_user_job_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserJobTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_job', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('job_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_job');
+    }
+}
