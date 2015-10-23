01ffcfd26781b56b803bd14ff63c92efee48afbf
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2015-10-24 00:10:28 +0100

    Move styles to the bottom, add sync status display

--- resources/assets/js/components/partials/header.js
+++ resources/assets/js/components/partials/header.js
@@ -1,8 +1,66 @@
 import React from 'react'
 import Style from 'react-postcss'
 import postcssPlugins from '../../postcss-plugins'
+import ReactInterval from 'react-interval'
+import Api from '../../utils/api'
 
 export default React.createClass({
+
+    getInitialState: function () {
+        return {
+            syncFinished: true,
+            checkQueue: {
+                enabled: true,
+                timeout: 5000
+            }
+        }
+    },
+
+    _checkQueue: function() {
+        Api.checkQueue((response) => {
+            this.setState({
+                checkQueue: {
+                    enabled: response.queue > 0,
+                    timeout: this.state.checkQueue.timeout
+                }
+            });
+        })
+    },
+
+    _renderSync: function() {
+        return !this.state.checkQueue.enabled ? (
+            <a className="component-header__menu__item__link" href="/sync">Sync</a>
+        ) : (
+            <span className="component-header__menu__item__link">
+                <img src="/img/spinner.gif" />
+            </span>
+        );
+    },
+
+    render: function () {
+        return (
+            <div className="component-header">
+                <Style plugins={postcssPlugins}>
+                    {this.style()}
+                </Style>
+                <ReactInterval {...this.state.checkQueue} callback={this._checkQueue} />
+                <div className="component-header__logo">
+                    <a className="component-header__logo__link" href="/#/repositories">Starred By Me</a>
+                </div>
+                <ul className="component-header__menu">
+                    <li className="component-header__menu__item">
+                        <a className="component-header__menu__item__link" href="/#/search">Search</a>
+                    </li>
+                    <li className="component-header__menu__item">
+                        <a className="component-header__menu__item__link" href="/#/repositories">Repositories</a>
+                    </li>
+                    <li className="component-header__menu__item">
+                        {this._renderSync()}
+                    </li>
+                </ul>
+            </div>
+        )
+    },
 
     style: function() {
         return `
@@ -46,29 +104,5 @@ export default React.createClass({
                 }
             }
         `
-    },
-
-    render: function () {
-        return (
-            <div className="component-header">
-                <Style plugins={postcssPlugins}>
-                    {this.style()}
-                </Style>
-                <div className="component-header__logo">
-                    <a className="component-header__logo__link" href="/#/repositories">Starred By Me</a>
-                </div>
-                <ul className="component-header__menu">
-                    <li className="component-header__menu__item">
-                        <a className="component-header__menu__item__link" href="/#/search">Search</a>
-                    </li>
-                    <li className="component-header__menu__item">
-                        <a className="component-header__menu__item__link" href="/#/repositories">Repositories</a>
-                    </li>
-                    <li className="component-header__menu__item">
-                        <a className="component-header__menu__item__link" href="/sync">Sync</a>
-                    </li>
-                </ul>
-            </div>
-        )
     }
 });
